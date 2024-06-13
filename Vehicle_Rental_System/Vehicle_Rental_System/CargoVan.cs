ff42c567798949f4fa64a01b025074edeed5f25e
DusanM998 <75371025+DusanM998@users.noreply.github.com>
2024-06-13 14:51:25 +0200

    Update CargoVan Class

--- Vehicle_Rental_System/Vehicle_Rental_System/CargoVan.cs
+++ Vehicle_Rental_System/Vehicle_Rental_System/CargoVan.cs
@@ -8,5 +8,19 @@ namespace Vehicle_Rental_System
 {
     internal class CargoVan : Vehicle
     {
+        public override decimal GetDailyRentalCost(int rentalDays)
+        {
+            return rentalDays <= 7 ? 50 : 40;
+        }
+
+        public override decimal GetDeilyInsuranceCost(Customer customer)
+        {
+            decimal cost = 0.0003m * Value;
+            if(customer.YearsOfExperience > 5) 
+            {
+                cost *= 0.85m;
+            }
+            return cost;
+        }
     }
 }
