5f91c1eaddc5d089e6eeeedb25cd280e5700453e
DusanM998 <75371025+DusanM998@users.noreply.github.com>
2024-06-13 14:45:47 +0200

    Adding initial classes

---
+++ Vehicle_Rental_System/Vehicle_Rental_System/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Rental_System
+{
+    public class Car : Vehicle
+    {
+        public int SafetyRating { get; set; }
+
+        public override decimal GetDailyRentalCost(int rentalDays)
+        {
+            return rentalDays <= 7 ? 20 : 15;
+        }
+        public override decimal GetDeilyInsuranceCost(Customer customer)
+        {
+            decimal cost = 0.0001m * Value;
+            if(SafetyRating >= 4)
+            {
+                cost *= 0.9m;
+            }
+            return cost;
+        }
+    }
+}
